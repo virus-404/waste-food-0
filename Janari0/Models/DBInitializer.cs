1de2620b8897e33b376f46ed2ed57086dddc1c05
Marc Felip <marc.felip.pomes@gmail.com>
2021-10-23 16:44:42 +0200

    Scaffolding + DBInicializer

--- Janari0/Models/DBInitializer.cs
+++ Janari0/Models/DBInitializer.cs
@@ -14,6 +14,8 @@ namespace Janari0.Models
 
             createRoles(roleManager);
             createDefaultUser(userManager);
+            createProduct(context);
+            createInitialState(context, userManager);
 
         }
         public enum Roles { Admin, User, Company };
@@ -50,6 +52,79 @@ namespace Janari0.Models
             
         }
 
-        
+
+        private static void createProduct(ApplicationDbContext context)
+        {
+            if (context.Products.Any())
+            {
+                return;
+            }
+
+            Product p1 = new Product
+            {
+                name = "Fuet",
+                brand = "Casa Tarradelles",
+            };
+            Product p2 = new Product
+            {
+                name = "Pa",
+                brand = "Bimbo",
+            };
+            Product p3 = new Product
+            {
+                name = "Llet",
+                brand = "Llet nostra",
+            };
+            Product p4 = new Product
+            {
+                name = "Galetes",
+                brand = "Oreo",
+            };
+            context.AddRange(p1, p2, p3, p4);
+            context.SaveChanges();
+
+        }
+
+        private static void createInitialState(ApplicationDbContext context, UserManager<UserApp> userManager)
+        {
+            var defaultUser = new UserApp
+            {
+                UserName = "user@user.com",
+                Email = "user@user.com",
+                firstName = "user",
+                lastName = "user2",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            var userexist = userManager.Users.Where(x => x.Email == defaultUser.Email).FirstOrDefault();
+            var user = userManager.FindByEmailAsync(defaultUser.Email);
+            if (userexist == null)
+            {
+                userManager.CreateAsync(defaultUser, "A@a1234567");
+                userManager.AddToRoleAsync(defaultUser, Roles.User.ToString());
+                Product p1 = context.Products.Where(x => x.name == "Llet").FirstOrDefault();
+                Product p2 = context.Products.Where(x => x.name == "Galetes").FirstOrDefault();
+
+                Stock s1 = new Stock {
+                    Id = defaultUser.Id,
+                    IDProduct = p1.IDProduct,
+                    expirationDate = new DateTime(2022, 12, 01),
+                    stock = 2,
+                };
+
+                Stock s2 = new Stock
+                {
+                    Id = defaultUser.Id,
+                    IDProduct = p2.IDProduct,
+                    expirationDate = new DateTime(2021, 10, 11),
+                    stock = 5,
+                };
+                context.AddRange(s1, s2);
+                context.SaveChanges();
+            }
+
+        }
+
+
     }
 }
