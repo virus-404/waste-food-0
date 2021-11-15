927fa6327de1eae8309bc9b014333254ca1e33bd
Sanel Novalić <sanel.novalic@edu.fit.ba>
2021-11-15 21:28:17 +0100

    Added a button dialogue for Add Photo button

--- Android/app/src/main/java/cat/udl/tidic/amb/janari0android/AddStockActivity.java
+++ Android/app/src/main/java/cat/udl/tidic/amb/janari0android/AddStockActivity.java
@@ -1,6 +1,8 @@
 package cat.udl.tidic.amb.janari0android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,22 @@ public class AddStockActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_stock);
+
+        final Button addPhoto = findViewById(R.id.addPhoto);
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Button gallery = (Button) findViewById(R.id.addGallery);
+                Button camera = (Button) findViewById(R.id.addCamera);
+                if(gallery.getVisibility()==View.INVISIBLE) {
+                    gallery.setVisibility(View.VISIBLE);
+                    camera.setVisibility(View.VISIBLE);
+                }
+                else{
+                    gallery.setVisibility(View.INVISIBLE);
+                    camera.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
