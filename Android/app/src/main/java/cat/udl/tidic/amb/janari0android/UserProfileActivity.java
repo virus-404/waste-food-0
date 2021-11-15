9856a1d7e23e1af6205478544bbac836a9a2c872
Sanel Novalić <sanel.novalic@edu.fit.ba>
2021-11-15 23:55:15 +0100

    Back button for Profile functionality added

--- Android/app/src/main/java/cat/udl/tidic/amb/janari0android/UserProfileActivity.java
+++ Android/app/src/main/java/cat/udl/tidic/amb/janari0android/UserProfileActivity.java
@@ -1,15 +1,28 @@
 package cat.udl.tidic.amb.janari0android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserProfileActivity extends AppCompatActivity {
 
+    private ImageButton goBack;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        goBack = (ImageButton) findViewById(R.id.goBackButton);
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
