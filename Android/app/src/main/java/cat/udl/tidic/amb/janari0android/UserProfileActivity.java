e6e9c55e67066449693e45ce4419c0ee69db29f4
Sanel Novalić <sanel.novalic@edu.fit.ba>
2021-11-17 00:10:23 +0100

    Sign out button works but now I can't register or login lol

--- Android/app/src/main/java/cat/udl/tidic/amb/janari0android/UserProfileActivity.java
+++ Android/app/src/main/java/cat/udl/tidic/amb/janari0android/UserProfileActivity.java
@@ -2,26 +2,48 @@ package cat.udl.tidic.amb.janari0android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class UserProfileActivity extends AppCompatActivity {
 
-    private ImageButton goBack;
+    private Button signOut;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        goBack = (ImageButton) findViewById(R.id.goBackButton);
+        TextView textEmail = (TextView) findViewById(R.id.profileEmail);
+        TextView textName = (TextView) findViewById(R.id.profileName);
+        textEmail.setText(user.getEmail());
+        textName.setText(user.getDisplayName());
+
+        ImageButton goBack = (ImageButton) findViewById(R.id.goBackButton);
+        signOut = findViewById(R.id.signOut);
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(UserProfileActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProfileActivity.this, LoginActivity.class);
+                startActivity(intent);
+                FirebaseAuth.getInstance().signOut();
             }
         });
     }
