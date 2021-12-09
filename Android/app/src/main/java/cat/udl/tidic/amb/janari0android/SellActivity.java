2c945b033a0cf9cad5ca8ab1e139c46a663b5478
edferoca <47976279+edferoca@users.noreply.github.com>
2021-12-09 16:20:55 +0100

    tutorial on main page

--- Android/app/src/main/java/cat/udl/tidic/amb/janari0android/SellActivity.java
+++ Android/app/src/main/java/cat/udl/tidic/amb/janari0android/SellActivity.java
@@ -90,12 +90,6 @@ public class SellActivity extends AppCompatActivity {
         buildRecyclerView();
         getSearchData();
 
-        tarjetadeprueba();
-
-
-
-
-
 
         inputPrice.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -222,41 +216,7 @@ public class SellActivity extends AppCompatActivity {
         });
     }
 
-    private void tarjetadeprueba() {
-        TapTargetView.showFor(this,                 // `this` is an Activity
-                TapTarget.forView(findViewById(R.id.inputPrice), "This is a target", "We have the best targets, believe me")
-                        // All options below are optional
-                        .outerCircleColor(R.color.colorBlack)      // Specify a color for the outer circle
-                        .outerCircleAlpha(0.96f)            // Specify the alpha amount for the outer circle
-                        .targetCircleColor(R.color.white)   // Specify a color for the target circle
-                        .titleTextSize(20)                  // Specify the size (in sp) of the title text
-                        .titleTextColor(R.color.white)      // Specify the color of the title text
-                        .descriptionTextSize(10)            // Specify the size (in sp) of the description text
-                        .descriptionTextColor(R.color.colorBlack)  // Specify the color of the description text
-                        .textColor(R.color.colorDarkGrey)            // Specify a color for both the title and description text
-                        .textTypeface(Typeface.SANS_SERIF)  // Specify a typeface for the text
-                        .dimColor(R.color.colorDarkGrey)            // If set, will dim behind the view with 30% opacity of the given color
-                        .drawShadow(true)                   // Whether to draw a drop shadow or not
-                        .cancelable(false)                  // Whether tapping outside the outer circle dismisses the view
-                        .tintTarget(true)                   // Whether to tint the target view's color
-                        .transparentTarget(false)           // Specify whether the target is transparent (displays the content underneath)
-                        //.icon(Drawable)                     // Specify a custom drawable to draw as the target
-                        .targetRadius(60),                  // Specify the target radius (in dp)
-                new TapTargetView.Listener() {          // The listener can listen for regular clicks, long clicks or cancels
-                    @Override
-                    public void onTargetClick(TapTargetView view) {
-                        super.onTargetClick(view);      // This call is optional
-                        doSomething();
-                    }
-                });
-    }
 
-    private void doSomething() {
-
-            Intent intent = new Intent(SellActivity.this, MainActivity.class);
-            startActivity(intent);
-
-    }
 
     private void showProductDetails(ProductSale productSale) {
         Intent intent = new Intent(SellActivity.this, ProductDetailsActivity.class);
