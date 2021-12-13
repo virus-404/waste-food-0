8095c230d2dd84e69473566b361a8e3b58b28470
edferoca <47976279+edferoca@users.noreply.github.com>
2021-12-13 21:01:20 +0100

    notifications from firebase  (1) build.grandle() AndroidManifest.xml NEW::: NotificationFirebaseMsg

---
+++ Android/app/src/main/java/cat/udl/tidic/amb/janari0android/NotificationFirebaseMsg.java
@@ -0,0 +1,6 @@
+package cat.udl.tidic.amb.janari0android;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class NotificationFirebaseMsg extends FirebaseMessagingService {
+}
