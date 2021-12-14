ad3bb034876e030d37289224350c7fe56e968a17
Sanel Novalić <sanel.novalic@edu.fit.ba>
2021-12-14 18:36:17 +0100

    Revert "Backgroud ok?"
    This reverts commit be91554094c725846ffa1e8e9f772c1716b91c9a.

--- Android/app/src/main/java/cat/udl/tidic/amb/janari0android/MainActivity.java
+++ Android/app/src/main/java/cat/udl/tidic/amb/janari0android/MainActivity.java
@@ -19,7 +19,6 @@ import android.content.pm.PackageManager;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -295,8 +294,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
     }
 
     private void getSliderData() {
