90a55ea33fff03b2378220928c35fa155904a029
edferoca <47976279+edferoca@users.noreply.github.com>
2021-12-15 07:33:21 +0100

    notification from firebase is ready

--- Android/app/src/main/java/cat/udl/tidic/amb/janari0android/NotificationFirebaseMsg.java
+++ Android/app/src/main/java/cat/udl/tidic/amb/janari0android/NotificationFirebaseMsg.java
@@ -1,6 +1,36 @@
 package cat.udl.tidic.amb.janari0android;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class NotificationFirebaseMsg extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if (remoteMessage.getNotification()!=null){
+            showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private void showNotification(String title, String body) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
+    }
 }
