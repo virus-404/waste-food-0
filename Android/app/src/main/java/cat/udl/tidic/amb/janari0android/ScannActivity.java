c00f035407508311f3dad12b624e89d9f744ac55
Sanel Novalić <sanel.novalic@edu.fit.ba>
2022-01-25 20:37:51 +0100

    Now scanned product have correct product names, big achievement

--- Android/app/src/main/java/cat/udl/tidic/amb/janari0android/ScannActivity.java
+++ Android/app/src/main/java/cat/udl/tidic/amb/janari0android/ScannActivity.java
@@ -27,15 +27,33 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 //import cat.udl.tidic.amb.janari0android.adapters;
 
 public class ScannActivity extends AppCompatActivity {
+    private static final String TAG = "bakedbeans";
     public boolean flag1;
-
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,19 +67,37 @@ public class ScannActivity extends AppCompatActivity {
         integrador.setBarcodeImageEnabled(true);
         integrador.initiateScan();
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-
         if (result != null)
             if (result.getContents() != null) {
                 Intent intent = new Intent(ScannActivity.this, AddStockProductNameActivity.class);
                 flag1 = true;
-                intent.putExtra("data01", result.getContents());
-                intent.putExtra("data02", flag1);
-                startActivity(intent);
+                Request getRequest = new Request.Builder()
+                        .url("https://world.openfoodfacts.org/api/v0/product/" + result.getContents() + ".json")
+                        .build();
+                client.newCall(getRequest).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        e.printStackTrace();
+                    }
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        String jsonData = response.body().string();
+                        JSONObject jsonObject = null;
+                        try {
+                            jsonObject = new JSONObject(jsonData);
+                            String productName = jsonObject.getJSONObject("product").getString("product_name");
+                            intent.putExtra("data01", productName);
+                            intent.putExtra("data02", flag1);
+                            startActivity(intent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
             else {
                 finish();
